0ced74ca3eaeb47d71990e7d913ee7868028fbe0
DSniper <singhdmgangian@gmail.com>
2024-11-03 18:40:26 +0530

    Adding Code for Dynamic Port Enablement

--- src/main/java/com/daisy/springBootHelloWorld/demo/DemoApplication.java
+++ src/main/java/com/daisy/springBootHelloWorld/demo/DemoApplication.java
@@ -3,15 +3,19 @@ package com.daisy.springBootHelloWorld.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 
 //https://start.spring.io/
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "9090"));
+		app.run(args);
 	}
 }
+
 
 
